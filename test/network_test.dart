2496630af909c454feeac848aa0212a901a3bbff
Joe Diragi <joyod3@gmail.com>
2019-04-16 22:49:44 -0400

    Implements actual network tests

--- test/network_test.dart
+++ test/network_test.dart
@@ -2,52 +2,22 @@ import 'package:dart_envirofacts/envirofacts.dart';
 import 'package:test/test.dart';
 
 void main() async {
-  group('make_url', () {
-    test('returns a valid URL from table', () {
-      var x = "${Network.URL}/SEMS_ACTIVE_SITES/";
-      expect(Network.make_url("SEMS_ACTIVE_SITES"), equalsIgnoringCase(x));
+  group('API call', () {
+    var api = EnvirofactsAPI.withEndpoint(ENDPOINT.semsActiveSites);
+    test('withEndpoint', () async {      
+      var response = await api.requestResults<SEMS>();
+      expect(response, hasLength(10));
     });
-    test('returns a valid URL from table, columnSort', () {
-      var x = "${Network.URL}/TABLE/COL/=/COL/";
-      expect(Network.make_url(
-        "TABLE",
-        columnName:"COL",
-        filterOperator: "=",
-        filterValue: "COL"
-      ), equalsIgnoringCase(x));
+    test('with responseCount', () async {
+      var response = await api.requestResults<SEMS>(resultCount: 13);
+      expect(response, hasLength(14)); // Must be 13 + 1 since we start at 0
     });
-    test('returns a valid URL for json format', () {
-      var x = "${Network.URL}/ENDPOINT/json";
-      expect(Network.make_url("ENDPOINT", format: "JSON"), equalsIgnoringCase(x));
+    test('with provided endpoint', () async {
+      var responseCustomEndpoint = await api.requestResults<SEMS>(
+        apiEndpoint: ENDPOINT.semsArchivedSites);
+      var responseNormalEndpoint = await api.requestResults<SEMS>();
+      expect(responseCustomEndpoint, hasLength(10));
+      expect(responseCustomEndpoint, isNot(equals(responseNormalEndpoint)));
     });
-    test('returns a valid URL for rowsToShow', () {
-      var x = "${Network.URL}/E/100:110/";
-      expect(Network.make_url("E", rowsToShow: [100, 110]), equalsIgnoringCase(x));
-    });
-    test('returns a valid URL for count', () {
-      var x = "${Network.URL}/E/COUNT/";
-      expect(Network.make_url("E", count: true), equalsIgnoringCase(x));
-    });
-    test('throws Error if format is invalid', () {
-      expect(() => Network.make_url("ENDPOINT", format: "jpeg"), throwsArgumentError);
-    });
-    test('throws Error if columnName is provided without filterOperator', () {
-      expect(() => Network.make_url("E", columnName: "E", filterValue: "E"), 
-        throwsArgumentError);
-    });
-    test('throws Error if columnName is provided without filterValue', () {
-      expect(() => Network.make_url("E", columnName: "E", filterOperator: "E"),
-        throwsArgumentError);
-    });
-    test('throws Error if filter provided withouth columnName', () {
-      expect(() => Network.make_url("E", filterValue: "E"), throwsArgumentError);
-    });
-    test('throws Error if rowsToShow is not a list', () {
-      expect(() => Network.make_url("E", rowsToShow: "E"), throwsArgumentError);
-    });
-    test('throws Error if rowsToShow is not of proper size', () {
-      expect(() => Network.make_url("E", rowsToShow: [0]), throwsArgumentError);
-    });
-  });
-  
+  });  
 }
